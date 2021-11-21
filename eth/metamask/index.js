ae7430b4415673d3115a86c363a81d1c7d0efef1
Sornram Saetang <waterfishX@outlook.com>
2021-11-21 23:58:10 +0700

    WIP: call contract

--- eth/metamask/index.js
+++ eth/metamask/index.js
@@ -1,26 +1,28 @@
 import { ethers } from "ethers";
 
-export const provider = window.ethereum && new ethers.providers.Web3Provider(window.ethereum)
+export const isClient = () => typeof window !== 'undefined'
 
-export const signer = provider && provider.getSigner()
+export const provider = isClient() && window.ethereum && new ethers.providers.Web3Provider(window.ethereum)
+
+export const signer = isClient() && provider && provider.getSigner()
 
 export const isMetaMaskInstalled = () => {
-    return Boolean(window.ethereum && window.ethereum.isMetaMask)
+    return isClient() && Boolean(window.ethereum && window.ethereum.isMetaMask)
 }
 
 export const isLoggedIn = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    const accounts = isClient() && await window.ethereum.request({ method: 'eth_accounts' });
     return !!accounts
 }
 
 export const getAccount = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    const accounts = isClient() && await window.ethereum.request({ method: 'eth_accounts' });
     return accounts[0]
 }  
 
 export const connectWallet = async (onError) => {
   try {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    isClient() && await window.ethereum.request({ method: 'eth_requestAccounts' });
   } catch (error) {
     onError?.(error.message)
   }
