edac4553bad019c530d2f68ba631a6ecfbbb62f2
Sornram Saetang <waterfishX@outlook.com>
2021-11-22 20:47:14 +0700

    change contract address

--- eth/metamask/Campaign.js
+++ eth/metamask/Campaign.js
@@ -7,4 +7,4 @@ export const CampaignContract = (address) => {
     return new ethers.Contract(address, JSON.parse(Campaign.interface), signer)
 }
 
-export const FactoryContract = () => new ethers.Contract('0xe3D6Edf10dF802E986Cca3ABAe882fBf837A56A2', JSON.parse(CampaignFactory.interface), signer)
+export const FactoryContract = () => new ethers.Contract('0x331020D6B9D654883899f41A2F6Aa1F108a3d77c', JSON.parse(CampaignFactory.interface), signer)
